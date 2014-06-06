7bd49345d63f016849a9270434b8c02ac991e73d
Alex Kerney <abk@mac.com>
2014-06-05 21:23:54 -0400

    Backing up via Dropbox ever 6 hours (if backup.py is running of course). Needs environmental variables set for DROPBOX_APP_KEY, DROPBOX_APP_SECRET, and DROPBOX_APP_TOKEN. Probably will explode and rain hellfire down if running and it tries to backup something other than a SQLite db.
    Closes #3

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'o4d?2EXxt9.62Qj;ghXm[{#voUmF#$7ExY8T^V8BRC8si8faA$'
-	
+	dropbox_app_key = os.environ.get('DROPBOX_APP_KEY')
+	dropbox_app_secret = os.environ.get('DROPBOX_APP_SECRET')
+	dropbox_app_token = os.environ.get('DROPBOX_APP_TOKEN')
 
 
 class DevelopmentConfig(Config):
@@ -12,6 +14,7 @@ class DevelopmentConfig(Config):
 		'name': 'main.db',
 		'engine': 'peewee.SqliteDatabase'
 	}
+	
 	
 class TestingConfig(Config):
 	TESTING = True
