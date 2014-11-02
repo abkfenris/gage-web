42e5905077983b03b6b740d6927b5ade348cdc32
Alex Kerney <abk@mac.com>
2014-11-02 10:26:46 -0500

    Should not be including tmp/coverage anymore

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -5,6 +5,7 @@ from app import create_app, db
 class BasicTestCase(unittest.TestCase):
 	def setUp(self):
 		self.app = create_app('testing')
+		self.client = self.app.test_client()
 		self.app_context = self.app.app_context()
 		self.app_context.push()
 		db.create_all()
