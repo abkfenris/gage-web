a708a5e15bfa98e5d44240599180cc5278bd0a44
Alex Kerney <abk@mac.com>
2014-11-02 09:25:26 -0500

    Basic tests, and testing via manage

--- manage.py
+++ manage.py
@@ -44,5 +44,16 @@ def list_routes():
 	for line in sorted(output):
 		print(line)
 
+@manager.command
+def test():
+	"""
+	Run the unit tests.
+	"""
+	import unittest
+	tests = unittest.TestLoader().discover('tests')
+	
+	unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 if __name__ == '__main__':
     manager.run()
