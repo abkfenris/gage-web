51915a96c0262a05f119cf5f0a530e4e9147d16b
Alex Kerney <abk@mac.com>
2014-11-02 09:40:00 -0500

    Added password functions to User and associated tests

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from geoalchemy2 import Geometry
 from geoalchemy2.shape import to_shape
 from sqlalchemy.dialects.postgresql import JSON
 from flask import url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 
 sections_regions = db.Table('sections_regions',
 	db.Column('section', db.Integer, db.ForeignKey('sections.id')),
@@ -40,10 +41,22 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(80), unique=True)
 	email = db.Column(db.String(120), unique=True)
+	password_hash = db.Column(db.String(128))
 	
-	def __init__(self, username, email):
-		self.username = username
-		self.email = email
+	@property
+	def password(self):
+		raise AttributeError('password is not a readable atribute')
+	
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+	
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
+	
+	# def __init__(self, username, email):
+	# 	self.username = username
+	# 	self.email = email
 	
 	def __repr__(self):
 		return '<User %r>' % self.username
