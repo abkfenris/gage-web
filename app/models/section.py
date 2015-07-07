62c1d4895da8deeb2b04368bea475b966f90df23
Alex Kerney <abk@mac.com>
2015-07-07 14:55:46 -0400

    Cleaned up built ins in section api

--- app/models/section.py
+++ app/models/section.py
@@ -99,7 +99,7 @@ class Section(db.Model):
                             slug=self.slug,
                             _external=True),
             'url': url_for('api.get_section',
-                           id=self.id,
+                           sid=self.id,
                            _external=True)
         }
         return json_section
@@ -114,7 +114,7 @@ class Section(db.Model):
             'name': self.name,
             'html': url_for('main.sectionpage',
                             slug=self.slug, _external=True),
-            'url': url_for('api.get_section', id=self.id, _external=True),
+            'url': url_for('api.get_section', sid=self.id, _external=True),
             'regions': [region.to_json() for region in self.regions],
             'sensors': [correlation.sensor.to_json()
                         for correlation in self.correlations],
